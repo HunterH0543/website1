54fc8fa26ab7c7eb9ba41a747d7328e3d5dda259
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-03-11 21:55:46 +0000

    Mon, Mar 11, 2024, 2:55 PM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,6 +1,12 @@
 toggle = docuemnt.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
 
 
 toggle.addEvenetListner('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
 })
