749ad5390aa889470f175c643e9cbe0352521297
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-03-14 20:54:59 +0000

    Thu, Mar 14, 2024, 1:54 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
 open = document.querySelectorAll('.link')
 
 open.forEach(link => {
-    link.addEventListener('click', (url) => {
-        openInNewTab('http://127.0.0.1:5500/website1/search.html')
-        openInNewTab('http://127.0.0.1:5500/website1/gallery.html')
-        openInNewTab('http://127.0.0.1:5500/website1/sound.html')
-        openInNewTab('http://127.0.0.1:5500/website1/loading.html')
-        openInNewTab('http://127.0.0.1:5500/website1/navigation.html')
-        openInNewTab('http://127.0.0.1:5500/website1/landingpage.html')
+    link.addEventListener('mouseover', () => {
+        removeActiveClasses()
+        link.classList.add('active')
     })
 })
-
-function openInNewTab(url) {
-    window.open(url, '_blank').focus();
-}
