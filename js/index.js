a246e4bc02e54b6230d99cb4b2aee5e3d9b5d746
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-03-14 21:00:47 +0000

    Thu, Mar 14, 2024, 2:00 PM -07:00

--- js/index.js
+++ js/index.js
@@ -5,7 +5,11 @@ link.addEventListener(
     (event) => {
       // highlight the mouseover target
       event.target.style.color = "purple";
+
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 1000);
     },
     false,
   );
-  Result
