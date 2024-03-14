089c52d3e7f8e3bc1bd812bbb1fcc0729d43c0e2
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-03-14 14:04:35 -0700

    added js for index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-const link = document.getElementById("link");
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
+const link6 = document.getElementById("link6");
 
-link.addEventListener(
+link1.addEventListener(
     "mouseover",
     (event) => {
       // highlight the mouseover target
@@ -10,6 +15,76 @@ link.addEventListener(
       setTimeout(() => {
         event.target.style.color = "";
       }, 1000);
+    },
+    false,
+  );
+
+  link2.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      event.target.style.color = "purple";
+
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 1000);
+    },
+    false,
+  );
+
+  link3.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      event.target.style.color = "purple";
+
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 1000);
+    },
+    false,
+  );
+
+  link4.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      event.target.style.color = "purple";
+
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 1000);
+    },
+    false,
+  );
+
+  link5.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      event.target.style.color = "purple";
+
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 1000);
+    },
+    false,
+  );
+
+  link6.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      event.target.style.color = "purple";
+
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 500);
     },
     false,
   );
